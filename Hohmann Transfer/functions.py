4734ae9ef3c20b1d66976d6c3d004f08a5877210
Milan Capoor <capoorm123@gmail.com>
2019-07-19 22:39:26 -0500

    fix patched conics

--- Hohmann Transfer/functions.py
+++ Hohmann Transfer/functions.py
@@ -2,7 +2,7 @@ import math as m
 
 body_mu = {'sun':132712000000, 'mercury':22032, 'venus':324860, 'earth':398600, 'mars':42828, 'jupiter':126687000, 'saturn':37931000, 'uranus':5794000, 'neptune':6835100}
 body_radius = {'sun':695700, 'mercury':2439.5, 'venus':6052, 'earth':6378, 'mars':3396, 'jupiter':71492, 'saturn':60268, 'uranus':25559, 'neptune':24764}
-orbital_radius = {'sun':0, 'mercury':57900000, 'venus':108100000, 'earth':149500000, 'mars':227800000, 'jupiter':778000000, 'saturn':1426000000, 'uranus':2868000000, 'neptune':4494000000}
+orbital_radius = {'sun':1, 'mercury':57900000, 'venus':108100000, 'earth':149500000, 'mars':227800000, 'jupiter':778000000, 'saturn':1426000000, 'uranus':2868000000, 'neptune':4494000000}
 body_mass = {'sun':333432, 'mercury':0.056, 'venus':0.817, 'earth':1, 'mars':0.108, 'jupiter':318, 'saturn':95.2, 'uranus':14.6, 'neptune':17.3}
 
 def get_altitude1(planet):
@@ -31,18 +31,18 @@ def get_mu(planet):
 
     return mu
 
-def TOF(a, x):
+def TOF(a):
     mu = get_mu('sun')
     TOF_sec = m.pi * m.sqrt((a ** 3)/ mu)
     if (TOF_sec > 86400):
         TOF_simp = TOF_sec / 86400
-        print(x, "The total time necessary to complete this maneuver is:", TOF_simp, "days")
+        print("The total time necessary to complete this maneuver is:", TOF_simp, "days")
     elif (TOF_sec > 3600):
         TOF_simp = TOF_sec / 3600
-        print(x, "The total time necessary to complete this maneuver is:", TOF_simp, "hours")
+        print("The total time necessary to complete this maneuver is:", TOF_simp, "hours")
     elif (TOF_sec > 60):
         TOF_simp = TOF_sec / 60
-        print(x, "The total time necessary to complete this maneuver is:", TOF_simp, "minutes")
+        print("The total time necessary to complete this maneuver is:", TOF_simp, "minutes")
     else:
-        print(x, "The total time necessary to complete this maneuver is:", TOF_sec, "seconds")
+        print("The total time necessary to complete this maneuver is:", TOF_sec, "seconds")
 
--- Hohmann Transfer/interplanetary.py
+++ Hohmann Transfer/interplanetary.py
@@ -1,89 +1,52 @@
-import functions
 import math as m
+import functions as f
 
-def get_planets():
-    planet1 = input("Which body are we starting from? ")
-    rp0 = float(input("What is the initial altitude of the parking radius? "))
-    planet2 = input("Which body are we going to? ")
-    rp1 = float(input("What is the altitude of the parking orbit on the target planet? "))
+mu_sun = f.get_mu('sun')
 
-    return planet1, planet2, rp0, rp1
+starting_planet = input("Which body are we starting from? ")
+starting_orbital_radius = float(input("What is the initial altitude of the parking orbit? ")) + f.body_radius[starting_planet]
 
-def get_epsilon(planet):
-    mu = functions.get_mu(planet)
-    epsilon = - mu / (r1 + r2)
-    return epsilon
-    
-planet = get_planets()
-planet1 = planet[0]
-planet2 = planet[1]
-origin_parking_radius = planet[2]
-target_parking_radius = planet[3]
+target_planet = input("Which body are we going to? ")
+target_orbital_radius = float(input("What is the altitude of the parking orbit on the target planet? ")) + f.body_radius[target_planet] #rpt
+print()
+r1 = f.orbital_radius[starting_planet]
+r2 = f.orbital_radius[target_planet]
 
-r1 = functions.get_orbital_radius(planet1)
-r2 = functions.get_orbital_radius(planet2)
 a = (r1 + r2) / 2
+c = (2 * a) - (2 * r1)
+eccentricity = c / a
 
-mu_sun = functions.get_mu('sun')
-epsilon_t = get_epsilon('sun')
+#Heliocentric
+transfer_energy = - mu_sun / (r1 + r2) #epsilon_t
+v1 = m.sqrt(2 * ((mu_sun / r1) + transfer_energy)) #heliocentric velocity
+v_starting_planet = m.sqrt(mu_sun / r1)
+v_escape_starting = abs(v1 - v_starting_planet) 
 
-def transfer():
-    #heliocentric
-    a = (r1 + r2) / 2
-    epsilon_origin = - (mu_sun - (2 * functions.body_radius[planet1]))
-    epsilon_transfer_orbit = - (mu_sun / (2 * a))
-    epsilon_target = (mu_sun / (2 * functions.body_radius[planet2]))
-    v_origin_planet = m.sqrt(mu_sun / r1)
-    v_target = m.sqrt(2 * ((mu_sun / r2) + epsilon_target))
-    v1 = m.sqrt(2 * ((mu_sun / r1) + epsilon_t))
-    v2 = m.sqrt(mu_sun / r1)
-    v_inf_origin = abs(v_target - v2)
-    v_inf_target = abs(v_target - v2)
-   
-    #departure
-    epsilon_inf_origin = (v_inf_origin ** 2) / 2
-    v_burnout_origin = m.sqrt(2 * ((functions.get_mu(planet1) / origin_parking_radius) + epsilon_inf_origin))
-    v_park_origin = m.sqrt(functions.get_mu(planet1) / origin_parking_radius)
-    delta_v_boost = abs(v_burnout_origin - v_park_origin)
+v2 = m.sqrt(2 * ((mu_sun / r2) + transfer_energy))
+target_energy = -(mu_sun / r2)
+target_velocity = m.sqrt(mu_sun / r2) #v_target
 
-    #arrival
-    epsilon_inf_target = (v_inf_target ** 2) / 2
-    v_burnout_target = m.sqrt(2 * ((functions.get_mu(planet2) + epsilon_inf_target)))
-    v_park_target = m.sqrt((functions.get_mu(planet2) / target_parking_radius) + epsilon_target)
-    delta_v_retro = abs(v_park_target - v_burnout_target)
+#Departure
+v_inf  = v1 - v_starting_planet
+escape_energy = (v_inf ** 2) / 2
+v0 = m.sqrt(2 * (f.get_mu(starting_planet) / starting_orbital_radius) + escape_energy)
 
-    delta_v_mission = delta_v_boost + delta_v_retro
+v_starting_park = m.sqrt(f.get_mu(starting_planet) / starting_orbital_radius)
+delta_v_boost = abs(v0 - v_starting_park)
 
+#Arrival
+v_inf_target = abs(target_velocity - v2)
 
-    if (v_target > v2):
-        print()
-        print("1) The craft will approach", planet2, "from the front")
-    else:
-        print()
-        print("1) The craft will approach", planet2, "from behind")    
+energy_target = ((v_inf_target ** 2) / 2)
 
-    target_circular_velocity = m.sqrt(functions.get_mu(planet2) / functions.body_radius[planet2])
+v_hyperbolic = m.sqrt(2 * ((f.get_mu(target_planet) / target_orbital_radius) + energy_target))
 
-    arrival_angular_momentum = functions.body_radius[planet2] * v_park_target
-    eccentricity = m.sqrt(1 + (2 * epsilon_inf_target * arrival_angular_momentum)/(functions.get_mu(planet2) ** 2))  
+v_target_park = m.sqrt(f.get_mu(target_planet) / target_orbital_radius)
+delta_v_retro = abs(v_target_park - v_hyperbolic)
 
-    print("2) The eccentricity of the target oribt is:", eccentricity)
-
-    if (eccentricity == 0):
-        print("3) The arrival orbit is circular ")
-    elif (eccentricity > 0 and eccentricity < 1):
-        print("3) The arrival orbit is elliptical ")
-    elif (eccentricity == 1):
-        print("3) The arrival orbit is parabolic ")
-    else:
-        print("3) The arrival orbit is hyperbolic ")
-
-    if (eccentricity != 0):
-        delta_v_circular = target_circular_velocity - v_park_target
-        print("4) The required velocity change to make the orbit circular is:", delta_v_circular/1000, "km/s")
-   
-    print("5) Total velocity change of the mission is:", (delta_v_mission + delta_v_circular)/1000, "km/s")
+#Total Change
+delta_v_mission = delta_v_boost + delta_v_retro
 
 def interplanetary():
-    transfer()
-    functions.TOF(a, '6)')
+    print("Total velocity change:", delta_v_mission, "km/s")
+    f.TOF(a)
